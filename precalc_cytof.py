0a968cc665857293928183d24302299bc30b0aec
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-10-24 15:14:21 -0400

    combined PMA and Plasma into single tsv (and made col-zscore version)

--- precalc_cytof.py
+++ precalc_cytof.py
@@ -1,6 +1,55 @@
 def main():
 
+  # combine_pma_plasma()
+
   precalc_col_zscore()
+
+
+def combine_pma_plasma():
+
+  from copy import deepcopy
+  import pandas as pd
+  from clustergrammer import Network
+
+  net_Plasma = deepcopy(Network())
+  net_PMA = deepcopy(Network())
+
+  net_Plasma.load_file('cytof_data/Plasma.txt')
+  net_PMA.load_file('cytof_data/PMA.txt')
+
+  tmp_df_Plasma = net_Plasma.dat_to_df()
+  df_Plasma = tmp_df_Plasma['mat']
+
+  tmp_df_PMA = net_PMA.dat_to_df()
+  df_PMA = tmp_df_PMA['mat']
+
+  print('Plasma shape '+ str(df_Plasma.shape))
+  print('PMA shape '+ str(df_PMA.shape))
+
+  # add categories to plasma cells
+  ####################################
+  rows_plasma = df_Plasma.index.tolist()
+  new_rows = []
+  for inst_row in rows_plasma:
+    inst_name = 'Cell: Plasma-'+inst_row
+    new_rows.append(inst_name)
+
+  df_Plasma.index = new_rows
+
+  # add categories to PMA cell s
+  rows_pma = df_PMA.index.tolist()
+  new_rows = []
+  for inst_row in rows_pma:
+    inst_name = 'Cell: PMA-'+inst_row
+    new_rows.append(inst_name)
+
+  df_PMA.index = new_rows
+
+  df_both = pd.concat([df_Plasma, df_PMA], axis=0)
+
+  df_both.to_csv('cytof_data/Plasma-PMA.txt', sep='\t')
+
+  print(df_both.shape)
 
 def precalc_col_zscore():
   '''
@@ -10,7 +59,7 @@ def precalc_col_zscore():
   from copy import deepcopy
   from clustergrammer import Network
 
-  data_types = ['Plasma', 'PMA']
+  data_types = ['Plasma', 'PMA', 'Plasma-PMA']
 
   for inst_data in data_types:
     net = deepcopy(Network())
