98665944e8e348143fc4a9c019bcbc343a63ba91
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-10-24 17:19:31 -0400

    did example downsampling

--- precalc_cytof.py
+++ precalc_cytof.py
@@ -6,7 +6,49 @@ def main():
 
   # precalc_col_zscore()
 
-  precalc_subsets()
+  # precalc_subsets()
+
+  precalc_downsample()
+
+def precalc_downsample():
+  from clustergrammer import Network
+  import numpy as np
+  import pandas as pd
+
+  net = Network()
+
+  net.load_file('cytof_data/Plasma-PMA_col-zscore.txt')
+  tmp_df = net.dat_to_df()
+  df = tmp_df['mat']
+
+  cols = df.columns.tolist()
+
+  X = df
+
+  from sklearn.cluster import MiniBatchKMeans
+
+  n_clusters = 500
+
+  # kmeans is run with rows as data-points and columns as dimensions
+  mbk = MiniBatchKMeans(init='k-means++', n_clusters=n_clusters,
+                        max_no_improvement=10, verbose=0)
+
+  # need to loop through each label (each k-means cluster) and count how many
+  # points were given this label. This will give the population size of each label
+  # For MNIST, I also need to get the digit breakdown of each cluster to see what
+  # digits make up each cluster. Then I can work on overrepresentation examples.
+  ################################################
+  mbk.fit(X)
+  mbk_means_labels = mbk.labels_
+  mbk_means_cluster_centers = mbk.cluster_centers_
+  mbk_means_labels_unique = np.unique(mbk_means_labels)
+
+  ds = mbk_means_cluster_centers
+
+  ds_df = pd.DataFrame(data=ds, columns = cols )
+
+  ds_df.to_csv('cytof_data/tmp_down.txt', sep='\t')
+
 
 def load_antibody_info():
   print('save antibody info as dictionary')
