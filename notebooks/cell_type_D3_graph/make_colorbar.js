cdedfab117fb18c659501d02c223989a1f3d2b3e
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-06-07 16:19:02 -0400

    made gradient from red to blue, working on adding text

--- notebooks/cell_type_D3_graph/make_colorbar.js
+++ notebooks/cell_type_D3_graph/make_colorbar.js
@@ -4,19 +4,45 @@ d3.select('#container-id-1 h1')
 var svg_height = 1000;
 var svg_width = 1000;
 
-var colorbar_width = 500;
-var colorbar_height = 10;
+var colorbar_width = 200;
+var colorbar_height = 20;
 
 var main_svg = d3.select('#container-id-1')
   .append('svg')
   .attr('height', svg_height + 'px')
   .attr('width', svg_height + 'px');
 
-// make background
+
+
+//Append a defs (for definition) element to your SVG
+var defs = main_svg.append("defs");
+
+//Append a linearGradient element to the defs and give it a unique id
+var linearGradient = defs.append("linearGradient")
+    .attr("id", "linear-gradient");
+
+
+//Set the color for the start (0%)
+linearGradient.append("stop")
+    .attr("offset", "0%")
+    .attr("stop-color", "blue");
+
+//Set the color for the end (100%)
+linearGradient.append("stop")
+    .attr("offset", "50%")
+    .attr("stop-color", "white");
+
+//Set the color for the end (100%)
+linearGradient.append("stop")
+    .attr("offset", "100%")
+    .attr("stop-color", "red");
+
+
+// make colorbar
 main_svg
   .append('rect')
   .classed('background', true)
   .attr('height', colorbar_height + 'px')
   .attr('width', colorbar_width + 'px')
-  .attr('fill', 'red')
+  .attr('fill', 'url(#linear-gradient)')
   .attr('transform', 'translate(10, 10)');
